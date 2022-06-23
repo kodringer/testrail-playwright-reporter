d8ac1c6c0156f4f86a6d75fa5c74058d793f28ba
Alexey Dolmatov <st.link@gmail.com>
2022-06-23 18:10:59 +0300

    some improve
    improve of reporter

--- reporter.testrail.ts
+++ reporter.testrail.ts
@@ -1,6 +1,7 @@
-import {Reporter, Suite, TestCase, TestResult, FullResult} from '@playwright/test/reporter';
+import {Reporter, Suite, FullResult} from '@playwright/test/reporter';
 import {FullConfig} from '@playwright/test';
-import TestRail from '@dlenroc/testrail';
+import TestRail, {ConfigItem} from '@dlenroc/testrail';
+import config from './playwright.config';
 require('dotenv').config();
 
 type TestRailTestResult = {
@@ -8,118 +9,115 @@ type TestRailTestResult = {
   status_id: number;
   comment?: string;
 };
+type TTestCaseResult = {
+  browser: string;
+  results: TestRailTestResult[];
+};
 
+// TestRail IDs
+const PROJECT_ID = 59;
+const DEFAULT_SUITE_ID = 849;
+const STATUS_MAPPING = {
+  passed: 1,
+  failed: 5,
+  timedOut: 5,
+  skipped: 3,
+};
+const BROWSERS: string[] = [];
 class TestRailReporter implements Reporter {
   private testRailApi: TestRail;
-  private projectId: number;
-  private testCaseResults: Array<TestRailTestResult>;
-  private suiteId: number;
-  private testPlanName: string;
-  private testPlanId: number | null;
-  private testRunId: number | null;
-  private testRunName: string;
-  private testPlanDescription: string;
+  private readonly projectId: number;
+  private suiteId: number | null;
+  private readonly testPlanName: string;
+  private readonly testPlanDescription: string;
+  private browserConfigList: ConfigItem[];
+  private testPlan: TestRail.Plan | null;
+  private suiteList: Suite[];
+  private testCaseResultList: TTestCaseResult[];
 
   constructor() {
-    this.projectId = 59;
-    this.suiteId = 849;
+    this.projectId = PROJECT_ID;
+    this.suiteId = null;
     this.testRailApi = new TestRail({
       host: this.getTestRailHost(),
       username: this.getTestRailUsername(),
       password: this.getTestRailPassword(),
     });
-    this.testCaseResults = [];
+    this.testCaseResultList = [];
     this.testPlanName = `Generated on ${new Date().toLocaleString()} for ${process.env.E2E_TESTS} tests with ${
       process.env.FL_ENV
     } backend`;
-    this.testRunId = null;
-    this.testPlanId = null;
-    this.testRunName = '';
     this.testPlanDescription = `Branch: ${process.env.BRANCH_NAME}`;
+    this.browserConfigList = [];
+    this.testPlan = null;
+    this.suiteList = [];
   }
 
-  // eslint-disable-next-line require-await
   async onBegin(config: FullConfig, suite: Suite) {
+    this.getBrowsers();
     console.log(`Starting the run with ${suite.allTests().length} tests`);
-  }
+    this.suiteList = suite.suites;
 
-  onTestEnd(test: TestCase, result: TestResult) {
-    const statusMapping = {
-      passed: 1,
-      failed: 5,
-      timedOut: 5,
-      skipped: 3,
-    };
+    const configList = await this.testRailApi.getConfigs(this.projectId);
+    const browserConfig = configList.filter(config => config.name.toLowerCase().includes('web browsers'));
 
-// test naming should be like "should add account => 58550 58552" for example.
-// in case of multiple suites - naming could be like "should add account => <suite ID> => <case id>"
-// in this case suiteIds = titleArray[1].split(' ') and caseIds = titleArray[2].split(' ')
-
-    if (test.title.includes('=>')) {
-      const titleArray = test.title.split(' => ');
-      const caseIds = titleArray[1].split(' ');
-      console.log('caseIds', caseIds);
-
-      if (caseIds != null) {
-        caseIds.forEach(caseId => {
-          this.testCaseResults.push({
-            case_id: Number(caseId),
-            status_id: statusMapping[result.status],
-            comment: `Test: ${test.title}; Duration: ${result.duration}ms`,
-          });
-        });
-      }
+    if (browserConfig && browserConfig.length) {
+      this.browserConfigList = browserConfig[0].configs;
     }
   }
 
   async onEnd(result: FullResult) {
     console.log(`Finished the run: ${result.status}`);
 
-    // console.log(this.testCaseResults);
-    // const configs = await this.testRailApi.getConfigs(this.projectId);
-    
-    await this.createTestPlan();
+    BROWSERS.forEach(browserName => {
+      this.testCaseResultList.push({
+        browser: browserName,
+        results: this.getBrowserResults(browserName),
+      });
+    });
 
-    // await this.addEntryToTestPlan(); 
+    // eslint-disable-next-line no-unused-expressions
+    process.env.TESTRAIL_PLAN_ID ? await this.useTestPlan() : await this.createTestPlan();
 
-    if (this.testRunId) {
-      await this.updateTestResults(this.testRunId);
+    for (const suite of this.suiteList) {
+      if (!this.testPlan) {
+        continue;
+      }
+
+      const testRun = this.testPlan.entries[0].runs.filter(run => run.config?.toLowerCase() === suite.title);
+      const testResult = this.testCaseResultList.find(result => result.browser === suite.title);
+
+      if (testRun[0].id && testResult) {
+        await this.updateTestResults(testRun[0].id, testResult.results);
+      }
     }
-  //uncomment next string if testRun should be closed
-  //  await this.closeTestRun(this.testRunId);
-  }
-
-  private async createTestRun() {
-    const testRunParams = {
-      include_all: true,
-      name: this.testRunName,
-      case_ids: this.getDistinctTestCaseIds(),
-      suite_id: this.suiteId,
-    };
-    const testRun = await this.testRailApi.addRun(this.projectId, testRunParams);
-    console.log(`Created TestRail Test Run id ${testRun.id}`);
-    return testRun.id;
   }
 
   private async createTestPlan() {
+    await this.getSuiteId();
+
+    const chromeId = Number(this.browserConfigList.filter(browser => browser.name.toLowerCase() === 'chrome')[0].id);
+    const edgeId = Number(this.browserConfigList.filter(browser => browser.name.toLowerCase() === 'edge')[0].id);
+    const firefoxId = Number(this.browserConfigList.filter(browser => browser.name.toLowerCase() === 'firefox')[0].id);
+
     const testPlanParams = {
       entries: [
         {
-          suite_id: this.suiteId,
+          suite_id: Number(this.suiteId),
           include_all: true,
-          config_ids: [48, 49, 50], //web browsers (Chrome, Edge, Firefox)
+          config_ids: [chromeId, edgeId, firefoxId],
           runs: [
             {
               include_all: true,
-              config_ids: [48], //Chrome
+              config_ids: [chromeId],
             },
             {
               include_all: true,
-              config_ids: [49], //Edge
+              config_ids: [edgeId],
             },
             {
               include_all: true,
-              config_ids: [50], //Firefox
+              config_ids: [firefoxId],
             },
           ],
         },
@@ -127,33 +125,20 @@ class TestRailReporter implements Reporter {
       name: this.testPlanName,
       description: this.testPlanDescription,
     };
-    const testPlan = await this.testRailApi.addPlan(this.projectId, testPlanParams);
-    console.log(`Created TestRail Test Plan id ${testPlan.id}`);
-    this.testPlanId = testPlan.id;
-    const chromeRun = testPlan.entries[0].runs.filter(run => run.config === 'Chrome'); // get needed testrun ID for update results
-    this.testRunId = chromeRun[0].id;
+
+    this.testPlan = await this.testRailApi.addPlan(this.projectId, testPlanParams);
+    console.log(`Created TestRail Test Plan id ${this.testPlan.id}`);
   }
 
-  // private async addEntryToTestPlan() {
-  //   const testRunParams = {
-  //     include_all: true,
-  //     suite_id: this.suiteId,
-  //   };
-  //   const testPlan = await this.testRailApi.addPlanEntry(this.testPlanId, testRunParams);
-  // }
-
-  private async closeTestRun(testRunId: number) {
-    await this.testRailApi.closeRun(testRunId);
+  private async useTestPlan() {
+    this.testPlan = await this.testRailApi.getPlan(Number(process.env.TESTRAIL_PLAN_ID));
+    console.log('Using existed testplan. ID:', process.env.TESTRAIL_PLAN_ID);
   }
 
-  private async updateTestResults(testRunId: number) {
-    await this.testRailApi.addResultsForCases(testRunId, {
-      results: this.testCaseResults,
+  private async updateTestResults(id: number, list: TestRailTestResult[]) {
+    await this.testRailApi.addResultsForCases(id, {
+      results: list,
     });
-  }
-
-  private getDistinctTestCaseIds() {
-    return [...new Set(this.testCaseResults.map(result => result.case_id))];
   }
 
   private getTestRailHost(): string {
@@ -162,6 +147,7 @@ class TestRailReporter implements Reporter {
     }
     throw new Error('TestRail host has not been defined in TESTRAIL_USERNAME environment variable.');
   }
+
   private getTestRailUsername(): string {
     if (process.env.TESTRAIL_USERNAME) {
       return process.env.TESTRAIL_USERNAME;
@@ -174,6 +160,58 @@ class TestRailReporter implements Reporter {
       return process.env.TESTRAIL_PASSWORD;
     }
     throw new Error('TestRail Password/ApiKey has not been defined in TESTRAIL_PASSWORD environment variable.');
+  }
+
+  private async getSuiteId() {
+    if (!this.suiteId) {
+      const suites = await this.testRailApi.getSuites(this.projectId);
+      const suite = suites.filter(suite => suite.name === 'Regression suite');
+
+      if (!suite || !suite.length) {
+        console.log('Couldn`t get Suite ID from Testrail');
+        return DEFAULT_SUITE_ID;
+      }
+
+      this.suiteId = Number(suite[0].id);
+      console.log('Suite ID:', this.suiteId);
+    }
+
+    return this.suiteId;
+  }
+
+  private getBrowserResults(browserName: string) {
+    const resultList: TestRailTestResult[] = [];
+
+    this.suiteList.forEach(async suite => {
+      if (suite.title === browserName) {
+        await suite.allTests().forEach(test => {
+          if (test.title.includes('=>')) {
+            const titleArray = test.title.split(' => ');
+            const caseIds = titleArray[1].split(' ');
+
+            test.results.forEach(result => {
+              if (caseIds != null) {
+                caseIds.forEach(caseId => {
+                  resultList.push({
+                    case_id: Number(caseId),
+                    status_id: STATUS_MAPPING[result.status],
+                    comment: `Test: ${test.title}; Duration: ${result.duration}ms`,
+                  });
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+
+    return resultList;
+  }
+
+  private getBrowsers() {
+    config.projects?.forEach(item => {
+      BROWSERS.push(item.name!);
+    });
   }
 }
 
